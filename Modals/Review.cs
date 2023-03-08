818dd1c36396909d06eeb38da80e92d749b0546d
Sharat Chandra <sharatchandra897@gmail.com>
2023-03-08 13:24:17 +0530

    Modals Addition

---
+++ Modals/Review.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PokemonReviewSys.Modals
+{
+	public class Review
+	{
+		public int id { get; set; }
+		public string Title { get; set; } = String.Empty;
+		public string Text { get; set; } = String.Empty;
+		public Pokemon? Pokemon { get; set; }
+        public Reviewer? Reviewer { get; set; }
+    }
+}
+
---
+++ Modals/Reviewer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PokemonReviewSys.Modals
+{
+	public class Reviewer
+	{
+        public int id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public ICollection<Review>? Reviews { get; set; }
+    }
+}
+

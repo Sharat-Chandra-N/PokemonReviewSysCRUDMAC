48e0e90cb8d02578db7e1dcc9a5bbd25b972c8f4
Sharat Chandra <sharatchandra897@gmail.com>
2023-03-18 13:10:26 +0530

    Data Context and Seed Addition

--- Modals/Owner.cs
+++ Modals/Owner.cs
@@ -4,8 +4,9 @@ namespace PokemonReviewSys.Modals
 	public class Owner
 	{
 		public int Id { get; set; }
-		public string Name { get; set; } = String.Empty;
-		public string Gym { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Gym { get; set; } = String.Empty;
 		public Country? Country { get; set; }
         public ICollection<PokemonOwner>? PokemonOwners { get; set; }
     }	

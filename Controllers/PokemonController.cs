27c1d9bc316095f6b27e66864244e2293911881e
Sharat Chandra <sharatchandra897@gmail.com>
2023-04-10 08:41:40 +0530

    Addition of get all pokemons

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewSys.Interfaces;
+using PokemonReviewSys.Modals;
+
+namespace PokemonReviewSys.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PokemonController : Controller
+	{
+		private readonly IPokemonRepository _pokemonRepository;
+
+		public PokemonController(IPokemonRepository pokemonRepository)
+		{
+			_pokemonRepository = pokemonRepository;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+		public IActionResult GetPokemons()
+		{
+			var pokemons = _pokemonRepository.GetPokemons();
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			return Ok(pokemons);
+		}
+	}
+}
+
